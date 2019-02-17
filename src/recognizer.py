74a78b11a297b5ca0b1ea80381d6c808a94b0e75
Wyatt Phillips <phillipsw1@wit.edu>
2019-02-17 00:07:18 -0500

    createProfile and uploadVideo appear stable

--- src/recognizer.py
+++ src/recognizer.py
@@ -56,7 +56,7 @@ class Recognizer:
            name (str): Full name of person's face being verified.
            image_dir (str): Path for all reference images.
         Returns:
-           bool: True if folder exists
+           bool: True if folder successful
         """
 
         # Checks if the provided image_dir is an existing folder
@@ -91,11 +91,11 @@ class Recognizer:
         Parameters:
            file_path (str): File path of reference image.
         Returns:
-           bool: True if file exists
+           bool: True if successful
         """
 
-        if os.path.isfile(file_path):
-            self.debugPrint("ERROR: File not found!")
+        if not os.path.isfile(file_path):
+            self.debugPrint("ERROR: Video not found!")
             return False
 
         self.video_path = file_path
@@ -104,9 +104,11 @@ class Recognizer:
 
         self.debugPrint("INFO: Processing frames...")
         self.video_frames = []
-        while video.isOpened():
+        ret = True
+
+        while ret:
             ret, frame = video.read()
-            self.video_frames.append(self, frame)
+            self.video_frames.append(frame)
 
         self.debugPrint("INFO: Video at '" + file_path + "' was uploaded successfully!")
         return True
@@ -118,7 +120,66 @@ class Recognizer:
         Parameters:
            showPreview (bool): If true, will pop-up a window showing the face analyses.
         Returns:
-           results (list):
-                frames_verified (int): The number of frames
+           results (dictionary):
+                percent (double): The average percentage of encoders that match
+                    the face with the greatest percentage in each frame
+                frame_percentages (list): The percentage of encoders that matched per frame.
+                frames_verified (int): The number of frames that detected a face
+                frames_total (int): Total number of frames in video
         """
 
+        #Initialize variables to return and initial buffers
+        frame_sums = []
+        percent = 0
+        frames_verified = 0
+        frames_total = len(self.video_frames)
+        frames_percentages = 0
+
+        face_locations = []
+        face_encodings = []
+
+        # Loops through each frame in the video
+        for frame in self.video_frames:
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            face_locations = fr.face_locations(rgb_small_frame)
+            face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
+
+            # Matches temporarily stores the sums of each fade match with an encoder
+            matches = []
+
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches.append(sum(fr.compare_faces(self.profile_encoders, face_encoding)))
+
+            if not matches:
+                frame_sums.append(-1)
+            else:
+                frame_sums.append(max(matches))
+
+            self.debugPrint("Frame " + len(frame_sums) + "matches with sum of: " + frame_sums[-1])
+
+        # This will contain only the frames that contained a face (not -1 sums)
+        validated_percents = []
+        for sum in frame_sums:
+            if sum != -1:
+                validated_percents.append(sum / len(self.profile_encoders))
+
+        # Set frames verified to length of validated sums
+        frames_percentages = len(validated_percents)
+
+        # obtain average percentage of face validation
+        percent = sum(frames_percentages) / len(frames_percentages)
+
+        self.debugPrint("INFO: Video processing complete with verification of: " + str(percent * 100) + "%")
+
+        results = {
+            "percent": percent,
+            "frames_verified": frames_verified,
+            "frames_total":  frames_total,
+            "frames_percentages": validated_percents
+        }
--- testing.py
+++ testing.py
@@ -1,4 +1,6 @@
 from src import recognizer
 
 test = recognizer.Recognizer(debug=True)
-test.createProfile("Barack Obama", "/home/phillipsw1/github/BrickHackV/img")
+test.createProfile("Barack Obama", "/home/phillipsw1/Downloads/test")
+test.uploadVideo("/home/phillipsw1/Downloads/test/cut.mp4")
+test.verify()
