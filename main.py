c83acd9a9d7374741972b33d9de115715b98aae2
Nova <noah.trauben@gmail.com>
2019-02-17 04:39:51 -0500

    All buttons on gui done

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import os
 from src import recognizer
 
 
-
 req = recognizer.Recognizer(True)
+
+
 def getProofVideo():
     filename = filedialog.askopenfilename(initialdir="/home", title="Select file",
                                           filetypes=(("jpeg files", "*.mp4"), ("all files", "*.*")))
@@ -15,9 +16,8 @@ def getProofVideo():
         req.uploadVideo(filename)
         videoButton.set(os.path.basename(filename))
 
+
 def getPhoto():
-    #filename = filedialog.askopenfilename(initialdir="/home", title="Select file",
-                                          #filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
     fileDir = filedialog.askdirectory()
     if fileDir == ():
         pass
@@ -26,18 +26,32 @@ def getPhoto():
         photoButton.set(os.path.basename(fileDir))
 
 
+def varify():
+    # messageBox.set(req.verify(showPreview=False))
+    data = {"percent": .90, "frame_percentages": [1,2,3,4,5,5], "frames_total": 45}
+    faceInFramePercent = str(data["percent"] * 100)
 
+    # data = req.verify(showPreview=False)
+    message = ( u"\u2713" + "Accuracy of video: {}%\nPercentage of frames with a face {} = {} ".format(faceInFramePercent
+        , (str(len(data["frame_percentages"])) + "/" + str(data["frames_total"])),
+        '{0:.3g}'.format((len(data["frame_percentages"]) / data["frames_total"]) * 100)))
+    messageBox.set(message)
+
+popup = False
 window = Tk()
-window.geometry("740x500")
-window.title("Title")
+window.configure(bg="#f58625")
+window.geometry("543x300")
+window.title("Face Verifier")
 videoButton = StringVar(value="Select Video")
 photoButton = StringVar(value="Select Photo Directory of format png or jpg")
-# Label(window, , font=("Ubuntu", 24), justify='center').pack()
+messageBox = StringVar()
 welcomeText = ("Please select a 'proof of life' video, and a historical photo \n"
-               "that will be used to test if the two are simular\n"
-               "For best performance, use a high resolution photo")
-Label(window, text=welcomeText, font=("ubuntu", 20), justify=CENTER, bg="SlateBlue2").grid(row=0, sticky=EW, columnspan=1)
-Button(window, textvariable=videoButton, command=getProofVideo).grid(row=1, column=0)
-Button(window, textvariable=photoButton, command=getPhoto).grid(row=2, column=0)
-Label(window)
+               "that will be used to test if the two are similar\n"
+               "For best performance, use high resolution photos")
+Label(window, text=welcomeText, font=("ubuntu", 15), justify=CENTER, bg="#17365d", fg="white").grid(row=0, sticky=EW, columnspan=1, pady=(0, 5))
+Button(window, textvariable=videoButton, command=getProofVideo).grid(row=1, column=0, pady=3)
+Button(window, textvariable=photoButton, command=getPhoto).grid(row=2, column=0, pady=3)
+Button(window, command=varify, text="Compute accuracy of video").grid(row=3, pady=3)
+Checkbutton(window, text="Show Popup Preview of the magic", variable=popup, bg="#f58625").grid(row=4, pady=3)
+Label(window, textvariable=messageBox, wraplength=450, font=("ubuntu", 12), bg="#f58625").grid(row=5, pady=3)
 window.mainloop()
